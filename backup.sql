e6092021713ed7eb8b20bdee2791d8bb1720300d
Daniel-Rue <78970069+Daniel-Rue@users.noreply.github.com>
2025-04-15 11:13:57 +0300

    Обновил дамп

--- backup.sql
+++ backup.sql
@@ -1,4 +1,4 @@
-docker-compose up -d db--
+--
 -- PostgreSQL database dump
 --
 
@@ -19,6 +19,41 @@ SET row_security = off;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: attributes; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.attributes (
+    id bigint NOT NULL,
+    name character varying(100) NOT NULL,
+    type character varying(20) NOT NULL,
+    unit character varying(50)
+);
+
+
+ALTER TABLE public.attributes OWNER TO postgres;
+
+--
+-- Name: attributes_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.attributes_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.attributes_id_seq OWNER TO postgres;
+
+--
+-- Name: attributes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.attributes_id_seq OWNED BY public.attributes.id;
+
 
 --
 -- Name: cart_items; Type: TABLE; Schema: public; Owner: postgres
@@ -196,6 +231,41 @@ ALTER SEQUENCE public.orders_id_seq OWNED BY public.orders.id;
 
 
 --
+-- Name: product_attribute_values; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.product_attribute_values (
+    id bigint NOT NULL,
+    value character varying(255) NOT NULL,
+    attribute_id bigint NOT NULL,
+    product_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.product_attribute_values OWNER TO postgres;
+
+--
+-- Name: product_attribute_values_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.product_attribute_values_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.product_attribute_values_id_seq OWNER TO postgres;
+
+--
+-- Name: product_attribute_values_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.product_attribute_values_id_seq OWNED BY public.product_attribute_values.id;
+
+
+--
 -- Name: product_category; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -246,6 +316,80 @@ ALTER SEQUENCE public.products_id_seq OWNED BY public.products.id;
 
 
 --
+-- Name: review_photos; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.review_photos (
+    id bigint NOT NULL,
+    image_url character varying(1024) NOT NULL,
+    uploaded_at timestamp without time zone,
+    review_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.review_photos OWNER TO postgres;
+
+--
+-- Name: review_photos_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.review_photos_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.review_photos_id_seq OWNER TO postgres;
+
+--
+-- Name: review_photos_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.review_photos_id_seq OWNED BY public.review_photos.id;
+
+
+--
+-- Name: reviews; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.reviews (
+    id bigint NOT NULL,
+    comment text,
+    created_at timestamp without time zone,
+    rating integer NOT NULL,
+    updated_at timestamp without time zone,
+    product_id bigint NOT NULL,
+    user_id bigint NOT NULL,
+    CONSTRAINT reviews_rating_check CHECK (((rating <= 5) AND (rating >= 1)))
+);
+
+
+ALTER TABLE public.reviews OWNER TO postgres;
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.reviews_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.reviews_id_seq OWNER TO postgres;
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.reviews_id_seq OWNED BY public.reviews.id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -285,6 +429,13 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: attributes id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.attributes ALTER COLUMN id SET DEFAULT nextval('public.attributes_id_seq'::regclass);
+
+
+--
 -- Name: cart_items id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -320,10 +471,31 @@ ALTER TABLE ONLY public.orders ALTER COLUMN id SET DEFAULT nextval('public.order
 
 
 --
+-- Name: product_attribute_values id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product_attribute_values ALTER COLUMN id SET DEFAULT nextval('public.product_attribute_values_id_seq'::regclass);
+
+
+--
 -- Name: products id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.products ALTER COLUMN id SET DEFAULT nextval('public.products_id_seq'::regclass);
+
+
+--
+-- Name: review_photos id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.review_photos ALTER COLUMN id SET DEFAULT nextval('public.review_photos_id_seq'::regclass);
+
+
+--
+-- Name: reviews id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews ALTER COLUMN id SET DEFAULT nextval('public.reviews_id_seq'::regclass);
 
 
 --
@@ -334,11 +506,21 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
+-- Data for Name: attributes; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.attributes (id, name, type, unit) FROM stdin;
+1	Цвет	STRING	\N
+2	Размер	NUMBER	см
+3	Водонепроницаемый	BOOLEAN	\N
+\.
+
+
+--
 -- Data for Name: cart_items; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.cart_items (id, quantity, cart_id, product_id) FROM stdin;
-12	3	3	3
 \.
 
 
@@ -347,7 +529,6 @@ COPY public.cart_items (id, quantity, cart_id, product_id) FROM stdin;
 --
 
 COPY public.carts (id, created_at, updated_at, user_id) FROM stdin;
-3	2025-04-06 14:54:07.031	2025-04-07 18:30:02.526	1
 \.
 
 
@@ -366,6 +547,7 @@ COPY public.categories (id, name, parent_id) FROM stdin;
 8	Чехлы	5
 9	Портативные колонки	2
 10	Наушники	2
+13	Ноутбуки	\N
 \.
 
 
@@ -374,8 +556,6 @@ COPY public.categories (id, name, parent_id) FROM stdin;
 --
 
 COPY public.order_items (id, price_at_order, quantity, order_id, product_id) FROM stdin;
-24	299.99	3	22	3
-25	899.99	1	23	2
 \.
 
 
@@ -384,8 +564,16 @@ COPY public.order_items (id, price_at_order, quantity, order_id, product_id) FRO
 --
 
 COPY public.orders (id, created_at, total_amount, user_id) FROM stdin;
-22	2025-04-06 15:01:02.026	899.97	1
-23	2025-04-07 16:39:48.872	899.99	1
+\.
+
+
+--
+-- Data for Name: product_attribute_values; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.product_attribute_values (id, value, attribute_id, product_id) FROM stdin;
+6	Синий	1	2
+7	9	2	2
 \.
 
 
@@ -402,6 +590,7 @@ COPY public.product_category (product_id, category_id) FROM stdin;
 7	7
 7	10
 26	10
+29	13
 \.
 
 
@@ -418,6 +607,26 @@ COPY public.products (id, base_price, description, discount_price, image_url, na
 4	899.99	\N	799.99	\N	Huawei P50	12
 3	299.99	\N	259.99	\N	Samsung Galaxy S8	5
 7	249.99	\N	219.99	\N	Apple AirPods Pro	15
+29	600.00	Мощный ноутбук с диагональю 17 дюймов	520.00	\N	Ноутбук Premium	0
+\.
+
+
+--
+-- Data for Name: review_photos; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.review_photos (id, image_url, uploaded_at, review_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: reviews; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.reviews (id, comment, created_at, rating, updated_at, product_id, user_id) FROM stdin;
+1	Хороший, вот фото.	2025-04-09 15:17:47.453	4	2025-04-09 15:17:47.453	2	1
+2	Отлично.	2025-04-09 15:23:10.526	5	2025-04-09 15:23:10.526	3	1
+3	Хороший, вот фото.	2025-04-09 22:56:37.011	4	2025-04-09 22:56:37.011	29	1
 \.
 
 
@@ -426,8 +635,15 @@ COPY public.products (id, base_price, description, discount_price, image_url, na
 --
 
 COPY public.users (id, created_at, email, first_name, last_name, middle_name, password, role) FROM stdin;
-1	2025-04-06 14:49:36.746	danil134567@yandex.ru	Данил	Кононенко	\N	$2a$10$wVMMGaoXQrz3shJzCv4KpuIMYdwLXvFhKT4EVoDjmKi3nY0zFYC4a	ROLE_USER
+1	2025-04-06 14:49:36.746	danil134567@yandex.ru	Данил	Кононенко	\N	$2a$10$wVMMGaoXQrz3shJzCv4KpuIMYdwLXvFhKT4EVoDjmKi3nY0zFYC4a	ROLE_ADMIN
 \.
+
+
+--
+-- Name: attributes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.attributes_id_seq', 3, true);
 
 
 --
@@ -448,7 +664,7 @@ SELECT pg_catalog.setval('public.carts_id_seq', 3, true);
 -- Name: categories_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.categories_id_seq', 10, true);
+SELECT pg_catalog.setval('public.categories_id_seq', 13, true);
 
 
 --
@@ -466,10 +682,31 @@ SELECT pg_catalog.setval('public.orders_id_seq', 23, true);
 
 
 --
+-- Name: product_attribute_values_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.product_attribute_values_id_seq', 6, true);
+
+
+--
 -- Name: products_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.products_id_seq', 26, true);
+SELECT pg_catalog.setval('public.products_id_seq', 29, true);
+
+
+--
+-- Name: review_photos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.review_photos_id_seq', 1, true);
+
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.reviews_id_seq', 3, true);
 
 
 --
@@ -477,6 +714,14 @@ SELECT pg_catalog.setval('public.products_id_seq', 26, true);
 --
 
 SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+
+
+--
+-- Name: attributes attributes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.attributes
+    ADD CONSTRAINT attributes_pkey PRIMARY KEY (id);
 
 
 --
@@ -520,6 +765,14 @@ ALTER TABLE ONLY public.orders
 
 
 --
+-- Name: product_attribute_values product_attribute_values_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product_attribute_values
+    ADD CONSTRAINT product_attribute_values_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: product_category product_category_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -536,6 +789,38 @@ ALTER TABLE ONLY public.products
 
 
 --
+-- Name: review_photos review_photos_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.review_photos
+    ADD CONSTRAINT review_photos_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: reviews reviews_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT reviews_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: reviews uk1nv3auyahyyy79hvtrcqgtfo9; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT uk1nv3auyahyyy79hvtrcqgtfo9 UNIQUE (user_id, product_id);
+
+
+--
+-- Name: product_attribute_values uk96c3i0xxtl9meai3fx4trcr1b; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product_attribute_values
+    ADD CONSTRAINT uk96c3i0xxtl9meai3fx4trcr1b UNIQUE (product_id, attribute_id);
+
+
+--
 -- Name: carts uk_64t7ox312pqal3p7fg9o503c2; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -549,6 +834,14 @@ ALTER TABLE ONLY public.carts
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT uk_6dotkott2kjsp8vw4d0m25fb7 UNIQUE (email);
+
+
+--
+-- Name: attributes uk_s9dywou66pe8gmb704v2jspr7; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.attributes
+    ADD CONSTRAINT uk_s9dywou66pe8gmb704v2jspr7 UNIQUE (name);
 
 
 --
@@ -584,6 +877,14 @@ ALTER TABLE ONLY public.product_category
 
 
 --
+-- Name: product_attribute_values fk9cv255c78bptiixa9axev9act; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product_attribute_values
+    ADD CONSTRAINT fk9cv255c78bptiixa9axev9act FOREIGN KEY (product_id) REFERENCES public.products(id);
+
+
+--
 -- Name: carts fkb5o626f86h46m4s7ms6ginnop; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -597,6 +898,22 @@ ALTER TABLE ONLY public.carts
 
 ALTER TABLE ONLY public.order_items
     ADD CONSTRAINT fkbioxgbv59vetrxe0ejfubep1w FOREIGN KEY (order_id) REFERENCES public.orders(id);
+
+
+--
+-- Name: reviews fkcgy7qjc1r99dp117y9en6lxye; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT fkcgy7qjc1r99dp117y9en6lxye FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: product_attribute_values fkdhipfhjpy3gq5wlo3vc2h8uf; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product_attribute_values
+    ADD CONSTRAINT fkdhipfhjpy3gq5wlo3vc2h8uf FOREIGN KEY (attribute_id) REFERENCES public.attributes(id);
 
 
 --
@@ -624,11 +941,27 @@ ALTER TABLE ONLY public.cart_items
 
 
 --
+-- Name: reviews fkpl51cejpw4gy5swfar8br9ngi; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT fkpl51cejpw4gy5swfar8br9ngi FOREIGN KEY (product_id) REFERENCES public.products(id);
+
+
+--
 -- Name: categories fksaok720gsu4u2wrgbk10b5n8d; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.categories
     ADD CONSTRAINT fksaok720gsu4u2wrgbk10b5n8d FOREIGN KEY (parent_id) REFERENCES public.categories(id);
+
+
+--
+-- Name: review_photos fkunrlxq8kevetatdevbd9xbp1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.review_photos
+    ADD CONSTRAINT fkunrlxq8kevetatdevbd9xbp1 FOREIGN KEY (review_id) REFERENCES public.reviews(id);
 
 
 --
